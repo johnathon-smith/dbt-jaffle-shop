f691b64127383da5887e117150370a91073ca13a
Johnathon Smith <smith.johnathon01@gmail.com>
2025-12-08 12:34:53 +0000

    Updated field names to be more understandable and readable.

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
+    status as payment_status,
 
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
